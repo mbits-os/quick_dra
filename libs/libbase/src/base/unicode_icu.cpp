5dced9def739febc1328213834e2c4b4ed280213
Marcin Zdun <marcin.zdun@gmail.com>
2026-02-04 15:28:12 +0100

    fix: add ICU-based implementation

---
+++ libs/libbase/src/base/unicode_icu.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 midnightBITS
+// This code is licensed under MIT license (see LICENSE for details)
+
+// #define U_DISABLE_RENAMING 1
+#include <unicode/unistr.h>
+
+#include <quick_dra/base/str.hpp>
+
+namespace quick_dra {
+	std::string to_upper(std::string_view input) {
+		std::string result{};
+		icu::UnicodeString::fromUTF8(input).toUpper().toUTF8String(result);
+		return result;
+	}
+}  // namespace quick_dra
