c843bce656d971cc1bb83825efe2a484ac6d2272
Marcin Zdun <marcin.zdun@gmail.com>
2026-02-23 14:19:56 +0100

    ci: lessen on coverage comment amount

--- .flow/extensions/quick_dra/test_coverage/cli/coveralls.py
+++ .flow/extensions/quick_dra/test_coverage/cli/coveralls.py
@@ -49,38 +49,58 @@ def _clean_old_comments(rt: env.Runtime):
     print(f"Removed {len(to_remove)} comment{'' if len(to_remove) == 1 else 's'}")
 
 
-def _comment_on_coverage(upload: str | None, build_num: str | None, rt: env.Runtime):
+def _comment_on_coverage(
+    build_num: str | None, coveralls_url: str | None, rt: env.Runtime
+):
     report_path: Path | None = None
-    if upload:
-        report_path = Path(upload)
-    elif build_num:
+    if build_num:
         report_path = Path("build", "artifacts", "coverage")
 
     if report_path:
         context = load_report(report_path, rt)
-        ctx = context.mustache_context(github_pr_new_code())
+        ctx = context.mustache_context(github_pr_new_code(), coveralls_url)
         print(CommentContext.render_terminal(ctx), file=sys.stderr)
         github_create_comment(CommentContext.render_html(ctx))
 
 
-def _finalize_parallel_report(build_num: str, repo_token: str | None, rt: env.Runtime):
-    if not repo_token:
-        return 0
-
-    url = f"{ENDPOINT}/webhook?{urlencode({"repo_token": repo_token})}"
-
-    payload = {"payload": {"build_num": build_num, "status": "done"}}
-
-    return rt.cmd(
+def _curl_post(url: str, body: str, rt: env.Runtime):
+    args = [
         "curl",
         "-X",
         "POST",
         "-H",
         "Content-Type: application/json",
         "-d",
-        json.dumps(payload, ensure_ascii=False),
+        body,
         url,
-    )
+    ]
+    rt.print(*args)
+    if rt.dry_run:
+        return None
+
+    proc = rt.capture(*args, silent=True)
+    if proc.returncode != 0:
+        print(
+            f"proj-flow: error: {args[0]} ended in failure, exiting",
+            file=sys.stderr,
+        )
+        raise SystemExit(1)
+
+    return json.loads(proc.stdout)
+
+
+def _finalize_parallel_report(build_num: str, repo_token: str | None, rt: env.Runtime):
+    if not repo_token:
+        return None
+
+    url = f"{ENDPOINT}/webhook?{urlencode({"repo_token": repo_token})}"
+
+    payload = {"payload": {"build_num": build_num, "status": "done"}}
+
+    response = _curl_post(url, json.dumps(payload), rt)
+    if isinstance(response, dict) and response.get("done") is True:
+        return cast(str, response.get("url"))
+    return None
 
 
 def _upload_parallel_report(output: str, repo_token: str | None, rt: env.Runtime):
@@ -113,13 +133,12 @@ def coveralls(
     if os.name == "nt":
         sys.stdout.reconfigure(encoding="utf-8")  # type: ignore
 
-    if build_num:
-        _clean_old_comments(rt)
-
-    _comment_on_coverage(upload, build_num, rt)
-
     if not upload and not build_num:
         print("error: one of --done and --upload is required")
+        return 1
+
+    if upload and build_num:
+        print("error: only one of --done or --upload is allowed")
         return 1
 
     repo_token = os.environ.get("COVERALLS_REPO_TOKEN")
@@ -128,9 +147,16 @@ def coveralls(
             "The $COVERALLS_REPO_TOKEN environment variable is missing. Not sending anything.",
             file=sys.stderr,
         )
-        return 0
+
+    coveralls_url: str | None = None
+    if build_num and repo_token:
+        coveralls_url = _finalize_parallel_report(build_num, repo_token, rt)
 
     if build_num:
-        return _finalize_parallel_report(build_num, repo_token, rt)
+        _comment_on_coverage(build_num, coveralls_url, rt)
+        return 0
+
+    if not repo_token:
+        return 0
 
     return _upload_parallel_report(cast(str, upload), repo_token, rt)
