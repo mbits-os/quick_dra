7a02f2d157509863fecc1d3bf5b5c6f15b98923c
Marcin Zdun <marcin.zdun@gmail.com>
2026-04-17 08:35:41 +0200

    test: cover gui_tool()

---
+++ libs/libgui/tests/mock_webui/webui/webui.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2026 midnightBITS
+// This code is licensed under MIT license (see LICENSE for details)
+
+#pragma once
+
+#include <cstdlib>
+
+static inline void* webui_malloc(size_t size) { return std::malloc(size); }
+static inline void webui_free(void* ptr) { return std::free(ptr); }
+static inline const char* webui_get_mime_type(const char*) { return "test/data"; }
