cd070a67cfa51926b0b10e85fb3efbe6bfdc0412
Marcin Zdun <marcin.zdun@gmail.com>
2026-06-21 14:15:26 +0200

    test: stabilize `Globals.config` and `Globals.configModified`

--- libs/gui/gui/tests/Globals.test.cpp
+++ libs/gui/gui/tests/Globals.test.cpp
@@ -4,86 +4,87 @@
 #include <gtest/gtest.h>
 #include "TestApp.hpp"
 
+namespace fs = std::filesystem;
 namespace quick_dra::gui::testing {
 	struct GlobEnv : Env {
-		size_t configurationChanged{};
-		size_t identifierChanged{};
-		size_t formSetChanged{};
-		size_t configModifiedChanged{};
+		QSignalSpy configModifiedSpy{&globals, &Globals::configModifiedChanged};
 
-		static auto cliTestDataDir() { return std::filesystem::path{cli_test_data}; }
+		static auto cliTestDataDir() { return fs::path{cli_test_data}; }
 		GlobEnv() {
-			{
-				auto in_file = std::ifstream{cliTestDataDir() / ".quick_dra.AB4123456_50671500000.quarter.yaml"sv};
-				auto out_file = std::ofstream{tmp_dir.cwd() / ".quick_dra.yaml"sv, std::ios::binary | std::ios::out};
-				out_file << in_file.rdbuf();
-			}
-			observeSignal(&Globals::configurationChanged, configurationChanged);
-			observeSignal(&Globals::identifierChanged, identifierChanged);
-			observeSignal(&Globals::formSetChanged, formSetChanged);
-			observeSignal(&Globals::configModifiedChanged, configModifiedChanged);
-		}
-
-		void observeSignal(void (Globals::*signal)(), size_t& counter) {
-			QObject::connect(&globals, signal, [&counter] { ++counter; });
-		}
-
-		void observeSignal(void (Globals::*signal)(bool), size_t& counter) {
-			QObject::connect(&globals, signal, [&counter] { ++counter; });
+			fs::copy_file(cliTestDataDir() / ".quick_dra.AB4123456_50671500000.quarter.yaml"sv,
+			              tmp_dir.cwd() / ".quick_dra.yaml"sv, fs::copy_options::overwrite_existing);
 		}
 	};
 
-#define EXPECT_CALLS(CONFIGURATION_COUNT, IDENTIFIER_COUNT, FORMSET_COUNT, CONFIGMODIFIED_COUNT) \
-	EXPECT_EQ(env.configurationChanged, CONFIGURATION_COUNT);                                    \
-	EXPECT_EQ(env.identifierChanged, IDENTIFIER_COUNT);                                          \
-	EXPECT_EQ(env.formSetChanged, FORMSET_COUNT);                                                \
-	EXPECT_EQ(env.configModifiedChanged, CONFIGMODIFIED_COUNT);                                  \
-	env.configurationChanged = env.identifierChanged = env.formSetChanged = env.configModifiedChanged = 0
+#define EXPECT_SIGNAL(SPY) EXPECT_TRUE(takeFirst(SPY))
+#define EXPECT_NO_SIGNAL(SPY)     \
+	do {                          \
+		SPY.wait(100ms);          \
+		EXPECT_TRUE(SPY.empty()); \
+	} while (false)
 
 	TEST(Globals, config) {
 		GlobEnv env{};
-		EXPECT_CALLS(0, 0, 0, 0);
+
+		QSignalSpy configurationSpy{&env.globals, &Globals::configurationChanged};
+		QSignalSpy formSetSpy{&env.globals, &Globals::formSetChanged};
 
 		env.globals.setConfig(env.tmp_dir.cwd() / ".quick_dra.yaml"sv, std::nullopt, false);
-		EXPECT_CALLS(1, 0, 1, 0);
+		EXPECT_SIGNAL(configurationSpy);
+		EXPECT_SIGNAL(formSetSpy);
 
 		auto payer = *env.globals.data().cfg.payer;
 		payer.first_name = "First"sv;
 		payer.last_name = "Last"sv;
 
 		env.globals.storePayer(payer);
-		EXPECT_CALLS(1, 0, 1, 0);
+		EXPECT_SIGNAL(configurationSpy);
+		EXPECT_SIGNAL(formSetSpy);
 
 		env.globals.storeInsured(env.globals.data().cfg.insured->size(), {});
-		EXPECT_CALLS(1, 0, 1, 0);
+		EXPECT_SIGNAL(configurationSpy);
+		EXPECT_SIGNAL(formSetSpy);
 
 		env.globals.storeInsured(env.globals.data().cfg.insured->size(), {});
-		EXPECT_CALLS(1, 0, 1, 0);
+		EXPECT_SIGNAL(configurationSpy);
+		EXPECT_SIGNAL(formSetSpy);
 
 		env.globals.storeInsured(env.globals.data().cfg.insured->size(), {});
-		EXPECT_CALLS(1, 0, 1, 0);
+		EXPECT_SIGNAL(configurationSpy);
+		EXPECT_SIGNAL(formSetSpy);
 
 		env.globals.storeInsured(0, {});
-		EXPECT_CALLS(1, 0, 1, 0);
+		EXPECT_SIGNAL(configurationSpy);
+		EXPECT_SIGNAL(formSetSpy);
 
 		env.globals.storeInsured(env.globals.data().cfg.insured->size() + 1, {});
-		EXPECT_CALLS(0, 0, 0, 0);
+		EXPECT_NO_SIGNAL(configurationSpy);
+		EXPECT_NO_SIGNAL(formSetSpy);
 
 		env.globals.removeInsured({0, env.globals.data().cfg.insured->size() - 1});
-		EXPECT_CALLS(1, 0, 1, 0);
+		EXPECT_SIGNAL(configurationSpy);
+		EXPECT_SIGNAL(formSetSpy);
 	}
 
 	TEST(Globals, configModified) {
 		GlobEnv env{};
 		EXPECT_FALSE(env.globals.configModified());
 
+		QSignalSpy configurationSpy{&env.globals, &Globals::configurationChanged};
+		QSignalSpy formSetSpy{&env.globals, &Globals::formSetChanged};
+		QSignalSpy configModifiedSpy{&env.globals, &Globals::configModifiedChanged};
+
 		env.globals.setConfig(env.tmp_dir.cwd() / ".quick_dra.yaml"sv, std::nullopt, false);
-		EXPECT_CALLS(1, 0, 1, 0);
+		EXPECT_SIGNAL(configurationSpy);
+		EXPECT_SIGNAL(formSetSpy);
+		EXPECT_NO_SIGNAL(configModifiedSpy);
 
 		auto const almost =
 		    QString::fromUtf8(as_sv((env.tmp_dir.cwd() / "name"sv / ".."sv / ".quick_dra.yaml"sv).u8string()));
 		env.globals.observedFileChanged(almost);  // same last write time
-		EXPECT_CALLS(0, 0, 0, 0);
+		EXPECT_NO_SIGNAL(configurationSpy);
+		EXPECT_NO_SIGNAL(formSetSpy);
+		EXPECT_NO_SIGNAL(configModifiedSpy);
 
 		{
 			auto out_file = std::ofstream{env.tmp_dir.cwd() / ".quick_dra.yaml"sv, std::ios::binary | std::ios::out};
@@ -91,18 +92,26 @@ namespace quick_dra::gui::testing {
 		}
 
 		env.globals.observedFileChanged(almost);
-		EXPECT_CALLS(0, 0, 0, 1);
+		EXPECT_SIGNAL(configModifiedSpy);
+		EXPECT_NO_SIGNAL(configurationSpy);
+		EXPECT_NO_SIGNAL(formSetSpy);
 		EXPECT_TRUE(env.globals.configModified());
 
 		env.globals.reloadConfig();
-		EXPECT_CALLS(1, 0, 1, 1);
+		EXPECT_SIGNAL(configurationSpy);
+		EXPECT_SIGNAL(formSetSpy);
+		EXPECT_SIGNAL(configModifiedSpy);
 		EXPECT_FALSE(env.globals.configModified());
 	}
 
 	TEST(Globals, identifier) {
 		GlobEnv env{};
+		QSignalSpy identifierSpy{&env.globals, &Globals::identifierChanged};
+		QSignalSpy formSetSpy{&env.globals, &Globals::formSetChanged};
+
 		env.globals.storeIdentifier({.index = 1, .date = 2020y / 5, .isOverriden = true});
-		EXPECT_CALLS(0, 1, 1, 0);
+		EXPECT_SIGNAL(identifierSpy);
+		EXPECT_SIGNAL(formSetSpy);
 #ifdef WIN32
 		static constexpr auto expected_settings = "[Settings]\nYearMonth=2020/5\nReportIndex=1\n"sv;
 #else
@@ -116,12 +125,14 @@ namespace quick_dra::gui::testing {
 		EXPECT_TRUE(globals2.reportId().isOverriden);
 
 		env.globals.storeIdentifier({.index = 1, .date = 2020y / 5, .isOverriden = false});
-		EXPECT_CALLS(0, 1, 1, 0);
+		EXPECT_SIGNAL(identifierSpy);
+		EXPECT_SIGNAL(formSetSpy);
 		EXPECT_EQ(fileContents(env.tmp_dir.cwd() / "config.ini"), "[Settings]\nReportIndex=1\n"sv);
 	}
 
 	TEST(Globals, settingsProvider) {
 		{
+			TestApp app{};
 			Globals globals{};
 			auto settings = globals.createSettings();
 			ASSERT_EQ(settings.format(), QSettings::NativeFormat);
