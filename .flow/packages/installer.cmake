dca225138e01a9e6f75e8792cc89ce57661b2aec
Marcin Zdun <marcin.zdun@gmail.com>
2026-07-10 11:29:39 +0200

    ci: do not pack libs/includes

--- .flow/packages/installer.cmake
+++ .flow/packages/installer.cmake
@@ -7,19 +7,9 @@ cpack_add_component_group(apps
     EXPANDED
 )
 
-cpack_add_component_group(devel
-    DISPLAY_NAME "Tworzenie oprogramowania"
-    DESCRIPTION "Narzędzia i biblioteki dla programistów"
-    EXPANDED
-)
-
 if (TARGET Qt6::Gui)
     cpack_add_component_group(qt6
         DESCRIPTION "Runtime for the Quick-DRA GUI application"
-    )
-
-    cpack_add_component_group(gui_devel
-        DESCRIPTION "Development tools and libraries for GUI application"
     )
 endif()
 
@@ -52,21 +42,7 @@ cpack_add_component(C004_data
     GROUP apps
 )
 
-cpack_add_component(libraries
-    DISPLAY_NAME "Biblioteki"
-    DESCRIPTION "Biblioteki i pliki nagłówkowe Quick-DRA"
-    DISABLED
-    GROUP devel
-)
-
 if (TARGET Qt6::Gui)
-    cpack_add_component(gui_libraries
-        DISPLAY_NAME "Biblioteki Quick-DRA GUI"
-        DESCRIPTION "Biblioteki i pliki nagłówkowe Quick-DRA GUI"
-        DISABLED
-        GROUP gui_devel
-    )
-
     cpack_configure_downloads(
         "https://github.com/mbits-os/quick_dra/releases/download/v${PROJECT_VERSION}/"
         UPLOAD_DIRECTORY "${PROJECT_BINARY_DIR}/packages/upload"
